062479678c4af0eb4fa16ca3c8513ae648a35971
Elviss0706 <templeder@gmail.com>
2026-06-10 17:12:00 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'baseball-v1';
+const FILES = [
+  './',
+  './棒球數據統計.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
